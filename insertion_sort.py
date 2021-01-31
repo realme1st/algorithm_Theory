bed75eaf2e478049ed436eb0325d191ea3540da2
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2021-02-01 00:24:04 +0900

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(data):
+    for index in range(len(data)-1):
+        for index2 in range(index+1,0,-1):
+            if data[index2]< data[index2-1]:
+                data[index2],data[index2-1] = data[index2-1],data[index2]
+            else:
+                break
+    return data
+
+import random
+data_list = random.sample(range(100),50)
+print(insertion_sort(data_list))
